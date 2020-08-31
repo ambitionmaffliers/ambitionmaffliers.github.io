cac2012cc0c06a8c5fcf8d7629a809d504b765da
ambitionmaffliers <70523332+ambitionmaffliers@users.noreply.github.com>
2020-08-31 17:07:48 +0200

    change default zoom

--- leaflet-example.js
+++ leaflet-example.js
@@ -8,7 +8,7 @@
  The source code below is the example from the leaflet start page.
  */
 
-var map = L.map('map').setView([49.07732194995885, 2.310714630297644], 17.5);
+var map = L.map('map').setView([49.07732194995885, 2.310714630297644], 13);
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
